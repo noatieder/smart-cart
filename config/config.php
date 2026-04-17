92390b51af1da06062bfc872386619ea79f9809c
Noa Tieder <noatieder@MacBookAir.lan>
2026-04-17 08:21:40 +0300

    Add and expose DB connection settings

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'noati2_SmartCart');
+define('DB_USER', 'noati2_noa');
+define('DB_PASS', 'noatieder3112');
+
+// You can add other configuration constants below
